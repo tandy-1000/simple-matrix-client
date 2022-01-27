5f9fef53bd6dff532a9c94aac83ee6d6ff38a662
tandy <tandy1000@cock.li>
2022-01-27 16:06:19 +0000

    add signin / register guest modal

--- src/home.nim
+++ src/home.nim
@@ -3,11 +3,32 @@ import
   pkg/matrix,
   shared
 
+proc signinModal: Vnode =
+  var loginDiv = buildHtml(tdiv())
+
+  result = buildHtml:
+    tdiv(class = "modal"):
+      button(id = "login", class = "text-button"):
+        text "Sign-in"
+        proc onclick() =
+          loginDiv = buildHtml(tdiv):
+            h3:
+              text "Login:"
+            input(id = "login-input", `type` = "text")
+            input(id = "login-input", `type` = "password")
+      p:
+        text "or"
+      button(id = "register", class = "text-button", ):
+        text "Register as Guest"
+      loginDiv
+      # elif guest:
+
 proc createDom: VNode =
   result = buildHtml:
     tdiv:
       headerSection()
-      main()
+      main:
+        signinModal()
       footerSection()
 
 setRenderer createDom
