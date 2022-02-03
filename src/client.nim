087e7ab34119027b15ffdb658fd5cf5dc638325b
tandy <tandy1000@cock.li>
2022-02-03 13:40:45 +0000

    add chat name on chat column

--- src/client.nim
+++ src/client.nim
@@ -205,7 +205,7 @@ proc chatPane*(userId: string, roomId: string): Vnode =
         renderNoneSelected()
       of ChatPaneView.selected:
         let joinedRoom = syncResp.rooms.join[roomId]
-        renderChatMessages(userId, joinedRoom)
+        renderChatMessages(roomId, userId, joinedRoom)
       tdiv(id = "message-box", class = "border-box"):
         tdiv(id = "message-input", autofocus = "autofocus", contenteditable = "true", onkeyupenter = send)
         button(id = "send-button", onclick = send):
