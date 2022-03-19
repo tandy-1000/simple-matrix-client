56cd574a4fb1ef999a1a86b631344b07637f18ca
tandy <tandy1000@cock.li>
2022-03-19 21:10:00 +0000

    move server under when not defined(js) block

--- src/server.nim
+++ src/server.nim
@@ -1,21 +1,21 @@
-import
-  pkg/prologue,
-  pkg/prologue/middlewares/[staticfile, cors],
-  urls
+when not defined(js):
+  import
+    pkg/prologue,
+    pkg/prologue/middlewares/[staticfile, cors],
+    urls
 
+  let
+    env = loadPrologueEnv(".env")
+    settings = newSettings(
+      appName = env.getOrDefault("appName", "Simple Matrix Client"),
+      debug = env.getOrDefault("debug", true),
+      port = Port(env.getOrDefault("port", 8080))
+    )
 
-let
-  env = loadPrologueEnv(".env")
-  settings = newSettings(
-    appName = env.getOrDefault("appName", "Simple Matrix Client"),
-    debug = env.getOrDefault("debug", true),
-    port = Port(env.getOrDefault("port", 8080))
-  )
+  var app = newApp(settings = settings)
 
-var app = newApp(settings = settings)
-
-app.use(staticFileMiddleware(env.get("staticDir")))
-app.use(CorsMiddleware(allowOrigins = @[env.getOrDefault("allowOrigins", "*")],
-  allowMethods = @[env.getOrDefault("allowMethods", "*")]))
-app.addRoute(urls.urlPatterns, "")
-app.run()
+  app.use(staticFileMiddleware(env.get("staticDir")))
+  app.use(CorsMiddleware(allowOrigins = @[env.getOrDefault("allowOrigins", "*")],
+    allowMethods = @[env.getOrDefault("allowMethods", "*")]))
+  app.addRoute(urls.urlPatterns, "")
+  app.run()
